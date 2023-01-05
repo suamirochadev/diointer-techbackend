9aa27877d9d8f90678218394885936db25b168a0
Suami Rocha <suamirochati@gmail.com>
2023-01-05 19:56:51 -0300

    variaveis e constantes

---
+++ primeiroProjeto/java-anatomia-classe/src/dio/aulas.backend/tipos.variaveis/tipos.variaveis.4/tiposVariaveis.java
@@ -0,0 +1,21 @@
+package dio.aulas.backend.tipos.variaveis.tipos.variaveis.4;
+
+public class tiposVariaveis {
+
+    public static void main(String[] args) throws Exception {
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
+        VALOR_DE_PI = 10.75;
+    }
+}
