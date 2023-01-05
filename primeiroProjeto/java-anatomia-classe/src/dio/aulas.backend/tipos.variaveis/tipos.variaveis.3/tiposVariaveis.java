4231e3e0c610fd3008317c8b17159aa8d87fc0c9
Suami Rocha <suamirochati@gmail.com>
2023-01-05 19:36:19 -0300

    casting

---
+++ primeiroProjeto/java-anatomia-classe/src/dio/aulas.backend/tipos.variaveis/tipos.variaveis.3/tiposVariaveis.java
@@ -0,0 +1,12 @@
+package dio.aulas.backend.tipos.variaveis.tipos.variaveis.3;
+
+public class tiposVariaveis {
+    // declaração de variaveis
+
+    double salarioMinimo = 2500;
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+}
