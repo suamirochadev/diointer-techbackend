28f66afab7d302cc1fcf857a8f457db59ceb155d
Suami Rocha <suamirochati@gmail.com>
2023-01-27 19:25:06 -0300

    desafio de código

---
+++ Desafio/src/SalvandoMusica/Program.java
@@ -0,0 +1,17 @@
+import java.util.Scanner; 
+
+public class Program {
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        String arquivoDoPc = leitor.next();
+
+        if(arquivoDoPc.contains(".mp3")){
+          System.out.println("Salvar");
+        }else {
+          System.out.println("Deletar");
+        }
+
+        // TODO: Crie a condição necessária para verificar o formato do arquivo de entrada
+
+    }
+}
